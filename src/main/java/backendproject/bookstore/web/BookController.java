e5d356f7221308e41c51ec8b11b4eef90f1ca8a2
CaisaZzZ <lappalainen.kaisae@gmail.com>
2026-09-13 12:37:45 +0300

    Database connected as a List

--- src/main/java/backendproject/bookstore/web/BookController.java
+++ src/main/java/backendproject/bookstore/web/BookController.java
@@ -1,13 +1,34 @@
 package backendproject.bookstore.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import backendproject.bookstore.domain.Book;
+import backendproject.bookstore.domain.BookRepository;
+
+
 
 @Controller
 public class BookController {
 
+    BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/index")
     public String bookController() {
         return "frontpage"; // frontpage.html
+    }
+
+    @GetMapping("/booklist")
+    public String bookController(Model model) {
+        List<Book> books =(List<Book>) bookRepository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
     }
 }
